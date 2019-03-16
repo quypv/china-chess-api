6eb1efb7894f18e926e93d057066ce4142d57bff
quypv <phamvanquy211@gmail.com>
2019-03-16 17:30:32 +0700

    Add Knight movement logic

--- src/troops/knight.js
+++ src/troops/knight.js
@@ -1,9 +1,121 @@
+const c = require('../constants')
+const p = require('../position')
 const Troop = require('./troop')
 
 class Knight extends Troop
 {
   constructor(code) {
     super(code)
+  }
+
+  /**
+   * See what moves this troop can take
+   * @param {Board} board 
+   */
+  calculateAvailableMoves(board) {
+    if (!this._pos) {
+      this._openMoves = []
+      return false
+    }
+
+    let upMoves = this.tryUp(board)
+    let downMoves = this.tryDown(board)
+    let leftMoves = this.tryLeft(board)
+    let rightMoves = this.tryRight(board)
+
+    this._openMoves = [].concat(upMoves, downMoves, leftMoves, rightMoves)
+  }
+
+  /**
+   * @param {Board} board 
+   * @return {array}
+   */
+  tryUp (board) {
+    let upPosL, upPosR
+    let blocker = p.up(this._pos)
+
+    if (blocker && !!board.at(blocker)) {
+      return []
+    }
+
+    let arrPos = []
+    if (upPosL = p.upLeft(this._pos, 2, 1))  arrPos.push(upPosL)
+    if (upPosR = p.upRight(this._pos, 2, 1)) arrPos.push(upPosR)
+
+    return this.try(arrPos, board)
+  }
+
+  /**
+   * @param {Board} board 
+   * @return {array}
+   */
+  tryDown (board) {
+    let downPosL, downPosR
+    let blocker = p.down(this._pos)
+
+    if (blocker && !!board.at(blocker)) {
+      return []
+    }
+
+    let arrPos = []
+    if (downPosL = p.downLeft(this._pos, 2, 1))  arrPos.push(downPosL)
+    if (downPosR = p.downRight(this._pos, 2, 1)) arrPos.push(downPosR)
+
+    return this.try(arrPos, board)
+  }
+
+  /**
+   * @param {Board} board 
+   * @return {array}
+   */
+  tryLeft (board) {
+    let leftPosL, leftPosR
+    let blocker = p.left(this._pos)
+
+    if (blocker && !!board.at(blocker)) {
+      return []
+    }
+
+    let arrPos = []
+    if (leftPosL = p.upLeft(this._pos, 1, 2))   arrPos.push(leftPosL)
+    if (leftPosR = p.downLeft(this._pos, 1, 2)) arrPos.push(leftPosR)
+
+    return this.try(arrPos, board)
+  }
+
+  /**
+   * @param {Board} board 
+   * @return {array}
+   */
+  tryRight (board) {
+    let rightPosL, rightPosR
+    let blocker = p.right(this._pos)
+
+    if (blocker && !!board.at(blocker)) {
+      return []
+    }
+
+    let arrPos = []
+    if (rightPosL = p.upRight(this._pos, 1, 2))   arrPos.push(rightPosL)
+    if (rightPosR = p.downRight(this._pos, 1, 2)) arrPos.push(rightPosR)
+
+    return this.try(arrPos, board)
+  }
+
+  /**
+   * @param {array} arrPos 
+   * @param {Board} board 
+   * @return {array}
+   */
+  try (arrPos, board) {
+    let moves = []
+
+    for (let pos of arrPos) {
+      if (this.allyAlreadyOnPos(pos, board)) continue
+      moves.push(pos)
+    }
+
+    return moves
   }
 }
 
---
+++ test/troops/knight.test.js
@@ -0,0 +1,83 @@
+const c = require('../../src/constants')
+const Board = require('../../src/game/board')
+const Render = require('../../src/game/render')
+var assert = require('assert')
+
+describe('Knight movement', () => {
+  let board = new Board()
+  board.init()
+
+  it('can jump up', () => {
+    board.reset()
+    assert.equal(board.move('B9', 'A7'), true)
+    assert.equal(board.move('H9', 'G7'), true)
+    board.reset()
+    assert.equal(board.move('B9', 'C7'), true)
+    assert.equal(board.move('H9', 'I7'), true)
+  })
+
+  it('can jump down', () => { 
+    board.reset()
+    assert.equal(board.move('B0', 'A2'), true)
+    assert.equal(board.move('H0', 'G2'), true)
+    board.reset()
+    assert.equal(board.move('B0', 'C2'), true)
+    assert.equal(board.move('H0', 'I2'), true)
+  })
+
+  it('can jump left', () => { 
+    board.reset()
+    board.forcePut('C4', board.take('B0'))
+    assert.equal(board.move('C4', 'A5'), true)
+  })
+
+  it('can jump right', () => { 
+    board.reset()
+    board.forcePut('C4', board.take('B0'))
+    assert.equal(board.move('C4', 'E5'), true)
+  })
+
+  it('can move continously', () => { 
+    board.reset()
+    assert.equal(board.move('B0', 'C2'), true)
+    assert.equal(board.move('C2', 'E1'), true)
+    assert.equal(board.move('E1', 'D3'), true)
+    assert.equal(board.move('D3', 'C5'), true)
+  })
+
+  it('can take down enemy', () => { 
+    board.reset()
+    board.move('B9', 'C7')
+    board.move('C7', 'E8')
+    board.move('E8', 'D6')
+    board.move('D6', 'C4')
+    assert.equal(board.move('C4', 'E3'), true)
+  })
+
+  it('cannot jump if path is blocked', () => { 
+    board.reset()
+    board.move('H9', 'G7')
+    assert.equal(board.move('G7', 'F5'), false)
+    assert.equal(board.move('G7', 'H5'), false)
+    assert.equal(board.move('G7', 'I6'), false)
+    assert.equal(board.move('G7', 'I8'), false)
+    assert.equal(board.move('G7', 'E6'), false)
+
+    board.forcePut('E4', board.take('G7'))
+    assert.equal(board.move('E4', 'F2'), false)
+    assert.equal(board.move('E4', 'D2'), false)
+  })
+
+  it('cannot jump outside the board', () => { 
+    board.reset()
+    board.move('H9', 'I7')
+    assert.equal(board.move('I7', 'J9'), false)
+  })
+
+  it('cannot jump if destination is blocked', () => { 
+    board.reset()
+    board.move('B0', 'C2')
+    assert.equal(board.move('C2', 'E3'), false)
+    assert.equal(board.move('C2', 'D0'), false)
+  })
+})

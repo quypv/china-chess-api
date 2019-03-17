f34816ac43fb119a4db990d19affb134633aa173
quypv <phamvanquy211@gmail.com>
2019-03-17 09:42:56 +0700

    optimize

--- src/troops/bishop.js
+++ src/troops/bishop.js
@@ -1,4 +1,5 @@
 const c = require('../constants')
+const p = require('../position')
 const Troop = require('./troop')
 
 class Bishop extends Troop
@@ -17,11 +18,8 @@ class Bishop extends Troop
       return false
     }
 
-    let x = this._pos.charAt(0)
-    let y = this._pos.charAt(1)
-    
-    let leftMoves = this.tryLeft(x, y, board)
-    let rightMoves = this.tryRight(x, y, board)
+    let leftMoves = this.tryLeft(board)
+    let rightMoves = this.tryRight(board)
 
     this._openMoves = [].concat(leftMoves, rightMoves)
   }
@@ -33,17 +31,15 @@ class Bishop extends Troop
    * @param {Board} board 
    * @return {array}
    */
-  tryLeft (x, y, board) {
+  tryLeft (board) {
     let arrPos = []
-    let leftX  = c.X_AXIS.indexOf(x) - 2
-    let upY    = c.Y_AXIS.indexOf(y) - 2
-    let downY  = c.Y_AXIS.indexOf(y) + 2
+    let upPos       = p.upLeft(this._pos, 2, 2)
+    let blockerUp   = p.upLeft(this._pos, 1, 1)
+    let downPos     = p.downLeft(this._pos, 2, 2)
+    let blockerDown = p.downLeft(this._pos, 1, 1)
 
-    if (leftX < 0) return arrPos
-    let toX = c.X_AXIS.charAt(leftX)
-
-    if (upY >= 0)                 arrPos.push(toX + c.Y_AXIS.charAt(upY))
-    if (downY < c.Y_AXIS.length)  arrPos.push(toX + c.Y_AXIS.charAt(downY))
+    if (upPos && !board.at(blockerUp))     arrPos.push(upPos)
+    if (downPos && !board.at(blockerDown)) arrPos.push(downPos)
 
     return this.try(arrPos, board)
   }
@@ -55,17 +51,15 @@ class Bishop extends Troop
    * @param {Board} board 
    * @return {array}
    */
-  tryRight (x, y, board) {
+  tryRight (board) {
     let arrPos = []
-    let rightX = c.X_AXIS.indexOf(x) + 2
-    let upY    = c.Y_AXIS.indexOf(y) - 2
-    let downY  = c.Y_AXIS.indexOf(y) + 2
+    let upPos       = p.upRight(this._pos, 2, 2)
+    let blockerUp   = p.upRight(this._pos, 1, 1)
+    let downPos     = p.downRight(this._pos, 2, 2)
+    let blockerDown = p.downRight(this._pos, 1, 1)
 
-    if (rightX >= c.X_AXIS.length) return arrPos
-    let toX = c.X_AXIS.charAt(rightX)
-
-    if (upY >= 0)                 arrPos.push(toX + c.Y_AXIS.charAt(upY))
-    if (downY < c.Y_AXIS.length)  arrPos.push(toX + c.Y_AXIS.charAt(downY))
+    if (upPos && !board.at(blockerUp))     arrPos.push(upPos)
+    if (downPos && !board.at(blockerDown)) arrPos.push(downPos)
 
     return this.try(arrPos, board)
   }
@@ -82,31 +76,11 @@ class Bishop extends Troop
     for (let pos of arrPos) {
       if (this.posCrossedRiver(pos)) continue
       if (this.allyAlreadyOnPos(pos, board)) continue
-      if (this.anyTroopBlockPath(this._pos, pos, board)) continue
 
       moves.push(pos)
     }
 
     return moves
-  }
-
-  /**
-   * @param {string} fromPos
-   * @param {string} toPos
-   * @return {boolean}
-   */
-  anyTroopBlockPath(fromPos, toPos, board) {
-    let ix = c.X_AXIS.indexOf(fromPos.charAt(0))
-    let iy = c.Y_AXIS.indexOf(fromPos.charAt(1))
-    let iToX = c.X_AXIS.indexOf(toPos.charAt(0))
-    let iToY = c.Y_AXIS.indexOf(toPos.charAt(1))
-
-    if (ix === iToX || iy === iToY) return false
-
-    let middleX = c.X_AXIS.charAt(ix > iToX ? ix-1 : ix+1)
-    let middleY = c.Y_AXIS.charAt(iy > iToY ? iy-1 : iy+1)
-
-    return !!board.at(middleX+middleY)
   }
 }
 
